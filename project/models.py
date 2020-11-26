f54523dc114d196bf0734ee08f7a8b6ac393cb37
dreamexplore <pravin99co.in@gmail.com>
2020-11-26 16:50:01 +0530

    Updates here

--- project/models.py
+++ project/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class project(models.Model):
+    image = models.Imagefield(upload_to='')
+    summary = models.Charfield(max_length=250)
+
